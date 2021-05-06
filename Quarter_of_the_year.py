b5bb89bafd72459487a45b19b21dc1670cfe75d3
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-05-06 14:10:30 +0100

    Create Quarter_of_the_year.py

---
+++ Quarter_of_the_year.py
@@ -0,0 +1,9 @@
+def quarter_of(month):
+    if month<=3 and month>0:
+        return 1
+    elif month>=4 and month<=6:
+        return 2
+    elif month>=7 and month<=9:
+        return 3
+    elif month>=10 and month<=12:
+        return 4
